0ce521ae0100477c10af5d49af54314d238c52ad
Arianna Ninh <ariannaninh@gmail.com>
2025-06-25 18:05:48 -0700

    Update index.js

--- 01-executing-files/index.js
+++ 01-executing-files/index.js
@@ -1,23 +1,4 @@
 // Run the following file with the node command in the terminal.
 
-const number = 29; // You can change this number to test others
-let isPrime = true;
-
-if (number === 1) {
-    console.log("1 is neither prime nor composite.");
-} else if (number > 1) {
-    for (let i = 2; i < number; i++) {
-        if (number % i === 0) {
-            isPrime = false;
-            break;
-        }
-    }
-
-    if (isPrime) {
-        console.log(`${number} is a prime number.`);
-    } else {
-        console.log(`${number} is not a prime number.`);
-    }
-} else {
-    console.log("The number is not a positive integer.");
-}
+const myName = "Laura"; // Change this name 
+console.log(`Hello, ${myName}! You just ran your first Node file!`)
