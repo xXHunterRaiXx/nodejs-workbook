79e6e114e816905fe37b58afa50078889da604c4
Arianna Ninh <ariannaninh@gmail.com>
2025-06-25 18:18:16 -0700

    Update reverseString.js

--- 01-executing-files/reverseString.js
+++ 01-executing-files/reverseString.js
@@ -1,6 +1,7 @@
 const input = "Javascript Rocks!" 
 
 // Your Task: 
-// Below, write Javascript code that reverses the input string. 
+// Below, write Javascript code that reverses the input string using a loop. 
 // Log the result using console.log(). 
-// Then run the file using the node command. You should see the reversed string logged in the terminal output.
+// Then run the file using the node command. 
+// If your code is correct, you should see the reversed string in the terminal output.
