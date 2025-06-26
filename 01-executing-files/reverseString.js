0aa65eb96309358c819e4fe1eb5a876ba6167884
Arianna Ninh <ariannaninh@gmail.com>
2025-06-25 18:16:49 -0700

    Create reverseString.js

---
+++ 01-executing-files/reverseString.js
@@ -0,0 +1,6 @@
+const input = "Javascript Rocks!" 
+
+// Your Task: 
+// Below, write Javascript code that reverses the input string. 
+// Log the result using console.log(). 
+// Then run the file using the node command. You should see the reversed string logged in the terminal output.
