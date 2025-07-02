f5e164dbb07bde7a035763c019231eb4f2c680ff
ninhja <ariannaninh@gmail.com>
2025-07-01 21:47:35 -0700

    updated string message in hello world tester

--- 01-executing-files/index.js
+++ 01-executing-files/index.js
@@ -1,4 +1,4 @@
 // Run the following file with the node command in the terminal.
 
-const myName = "Laura"; // Change this name 
-console.log(`Hello, ${myName}! You just ran your first Node file!`)
+const myName = "Laura"; // Change this name
+console.log(`Hello ${myName}! Welcome to Node.js!`);
