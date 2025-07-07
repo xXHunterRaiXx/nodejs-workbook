67d8cceeae6fc09353b4726e59c41ded20703a25
Arianna Ninh <ariannaninh@gmail.com>
2025-07-07 11:20:34 -0700

    Update birthstoneFinder.js

--- 03-modules-and-files/birthstoneFinder.js
+++ 03-modules-and-files/birthstoneFinder.js
@@ -7,5 +7,5 @@ REQUIREMENTS:
 - Your program should accept 1 user input: a year (such as "January")
 - Your program should output a console.log() message that says the month's birthstone, such as...
     - "The birthstone for January is Garnet."
-    - "The birthstone for July is Ruby."
+    - "The birthstone for July is Ruby." 
 */
