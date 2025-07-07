c7d7393295c6c0ae82c747dff87b06420321d5f3
ninhja <ariannaninh@gmail.com>
2025-07-06 23:49:08 -0700

    created monday materials

---
+++ 03-modules-and-files/birthstoneFinder.js
@@ -0,0 +1,11 @@
+/* 
+YOUR TASK: 
+Create a program that determines the birthstone for a month inputted by the user. 
+Your app should read birthstone data from the data.json file.
+
+REQUIREMENTS:
+- Your program should accept 1 user input: a year (such as "January")
+- Your program should output a console.log() message that says the month's birthstone, such as...
+    - "The birthstone for January is Garnet."
+    - "The birthstone for July is Ruby."
+*/
