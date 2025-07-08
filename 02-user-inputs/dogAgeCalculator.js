901e22a84ec6bbb9cc8e8cb35acbe8ad3a2ac3e1
xXHunterRaiXx <hunter.rai1221@gmail.com>
2025-07-08 12:00:47 -0500

    updated dogagecalculator and finished it

--- 02-user-inputs/dogAgeCalculator.js
+++ 02-user-inputs/dogAgeCalculator.js
@@ -8,23 +8,32 @@ Run this file with the node command:
 node dogAgeCalculator.js
 
 */
-let dogName = "Rocket";
-let humanAge = 5;
+let dogName = process.argv[2];
+let humanAge = Number(process.argv[3]);
 
 let oneDogYear = 15;
 let twoDogYear = oneDogYear + 9;
-let threePlusDogYears = twoDogYear + 5;
+let threePlusDogYears = twoDogYear + (humanAge - 2) * 5;
+console.log(
+  `${dogName} is ${humanAge} years old in human years and ${threePlusDogYears} in dog years`
+);
+//   );)
+//one year = 15
+// two = 15 + 9
+//three years 15 + 9 + 5
+//four years is 15 + 9 + 5 + 5
+//five years is 15 + 9 + 5 + 5 + 5
 
-if (humanAge === 1) {
-  console.log(
-    `${dogName} is ${humanAge} years old in human years and ${oneDogYears} in dog years`
-  );
-}
+// if (humanAge === 1) {
+//   console.log(
+//     `${dogName} is ${humanAge} years old in human years and ${oneDogYears} in dog years`
+//   );
+// }
 
-if (humanAge === 2) {
-  console.log(
-    `${dogName} is ${humanAge} years old in human years and ${twoDogYearDogYears} in dog years`
-  );
-} else {
-  console.log(`${threePlusDogYears} in dog years `);
-}
+// if (humanAge === 2) {
+//   console.log(
+//     `${dogName} is ${humanAge} years old in human years and ${twoDogYearDogYears} in dog years`
+//   );
+// } else {
+//   console.log(`${threePlusDogYears} in dog years `);
+// }
