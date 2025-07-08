fdef8a576a8e753448e2a74e915b0d0db09ac844
xXHunterRaiXx <hunter.rai1221@gmail.com>
2025-07-08 11:20:42 -0500

    added git config

--- 02-user-inputs/dogAgeCalculator.js
+++ 02-user-inputs/dogAgeCalculator.js
@@ -14,3 +14,17 @@ let humanAge = 5;
 let oneDogYear = 15;
 let twoDogYear = oneDogYear + 9;
 let threePlusDogYears = twoDogYear + 5;
+
+if (humanAge === 1) {
+  console.log(
+    `${dogName} is ${humanAge} years old in human years and ${oneDogYears} in dog years`
+  );
+}
+
+if (humanAge === 2) {
+  console.log(
+    `${dogName} is ${humanAge} years old in human years and ${twoDogYearDogYears} in dog years`
+  );
+} else {
+  console.log(`${threePlusDogYears} in dog years `);
+}
--- 03-modules-and-files/birthstoneFinder.js
+++ 03-modules-and-files/birthstoneFinder.js
@@ -1,11 +1,21 @@
-/* 
-YOUR TASK: 
-Create a program that determines the birthstone for a month inputted by the user. 
-Your app should read birthstone data from the data.json file.
+function getBirthstone() {
+  // we're gonna use the readFile method to read from the data.json file
+  // this method takes in 3 parameters:
+  // 1. The file we want to read
+  // 2. the way the file is encoded
+  // 3. the function we run once we've read the file
+  fs.readFile("./data.json", "utf8", (err, data) => {
+    // console.log(data);
+    const birthstoneData = JSON.parse(data);
 
-REQUIREMENTS:
-- Your program should accept 1 user input: a year (such as "January")
-- Your program should output a console.log() message that says the month's birthstone, such as...
-    - "The birthstone for January is Garnet."
-    - "The birthstone for July is Ruby." 
-*/
+    // tell the user their birthstone
+    if (birthstoneData[month]) {
+      console.log(`The birthstone for ${month} is ${birthstoneData[month]}`);
+    } else {
+      console.log("Invalid month. Please enter a valid month!");
+    }
+  });
+}
+
+// run my function
+getBirthstone();
