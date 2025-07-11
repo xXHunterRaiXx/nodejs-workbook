fd176fb83331946c8cab6a2b90884cb55e5419ce
ninhja <ariannaninh@gmail.com>
2025-07-11 13:29:04 -0700

    created animals API project for learning express with SQL

---
+++ 07-animals-API-server-SQL/src/index.js
@@ -0,0 +1,23 @@
+// Animals API
+
+// ---------------------------------
+// Boilerplate Code to Set Up Server
+// ---------------------------------
+
+// ---------------------------------
+// Helper Functions
+// ---------------------------------
+
+// ---------------------------------
+// API Endpoints
+// ---------------------------------
+
+// GET /get-all-animals
+
+// GET /get-one-animal/:name
+
+// GET /delete-one-animal/:name
+
+// POST /add-one-animal
+
+// POST /update-one-animal
